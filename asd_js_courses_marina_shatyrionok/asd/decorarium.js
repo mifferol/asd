13d5178465d86c24839d384b05214a8d89907727
mifferol <marishaaa20027@gmail.com>
2020-02-28 19:34:16 +0200

    Add files via upload

---
+++ asd_js_courses_marina_shatyrionok/asd/decorarium.js
@@ -0,0 +1,8 @@
+$("#id1").click(function(){
+	$("#list2").addClass("hidden");
+	$("#list1").removeClass("hidden");
+})
+$("#id2").click(function(){
+	$("#list1").addClass("hidden");
+	$("#list2").removeClass("hidden");
+})
