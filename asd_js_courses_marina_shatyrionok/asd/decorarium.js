9c8067efc8777327df5f272e7190022281c73f30
mifferol <marishaaa20027@gmail.com>
2020-02-29 13:37:32 +0200

    Delete decorarium.js

--- asd_js_courses_marina_shatyrionok/asd/decorarium.js
+++
@@ -1,8 +0,0 @@
-$("#id1").click(function(){
-	$("#list2").addClass("hidden");
-	$("#list1").removeClass("hidden");
-})
-$("#id2").click(function(){
-	$("#list1").addClass("hidden");
-	$("#list2").removeClass("hidden");
-})
